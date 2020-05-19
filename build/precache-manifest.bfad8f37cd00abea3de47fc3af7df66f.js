9ed2d8e379c22c8fa26782e13d20c6d9f870cca9
amjadali111 <amjadali101.mit@gmail.com>
2020-05-19 21:59:21 +0500

    all folders added

---
+++ build/precache-manifest.bfad8f37cd00abea3de47fc3af7df66f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7466c897e4fd6360f747ecef5f7a9b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab5acf611f12ee75747",
+    "url": "/static/css/main.e2b28a30.chunk.css"
+  },
+  {
+    "revision": "b5fa271080307ebbaa3b",
+    "url": "/static/js/2.a3ddd64c.chunk.js"
+  },
+  {
+    "revision": "1ab5acf611f12ee75747",
+    "url": "/static/js/main.9a06e1bd.chunk.js"
+  },
+  {
+    "revision": "4124f1c6827423b29904",
+    "url": "/static/js/runtime~main.25947c66.js"
+  }
+]);
